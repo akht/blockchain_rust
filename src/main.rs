d3b04d79788d8b102cac5b71cb3b4e94e0de649b
akht <picopeko@gmail.com>
2019-01-11 19:26:12 +0900

    new_transactionとlast_blockの実装中
    まだコンパイルエラーになる

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 extern crate iron;
 extern crate router;
+extern crate crypto;
+extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 use std::io::Read;
+use std::collections::HashSet;
+use std::time::SystemTime;
+use rustc_serialize::hex::ToHex;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
 
 fn main() {
+    let blockchain = Blockchain::new();
+    println!("{:?}", blockchain);
+
+
+
     let mut router = Router::new();
 
     router.get("/", index, "index");
@@ -47,4 +59,89 @@ fn main() {
     }
 
     Iron::new(router).http("localhost:3000").unwrap();
+}
+
+#[derive(Debug)]
+struct Blockchain {
+    chain: Vec<Block>,
+    nodes: HashSet<String>,
+    current_transactions: Vec<Transaction>,
+}
+
+impl Blockchain {
+    fn new() -> Blockchain {
+        let mut blockchain = Blockchain {
+            chain: Vec::new(),
+            nodes: HashSet::new(),
+            current_transactions: Vec::new(),
+        };
+        blockchain.new_block(100, Some(String::from("1")));
+
+        blockchain
+    }
+
+    fn new_block(&mut self, proof: usize, previous_hash: Option<String>) -> Block {
+        let block = Block {
+            index: self.chain.len() + 1,
+            timestamp: SystemTime::now(),
+            transactions: self.current_transactions.to_vec(),
+            proof: proof,
+            previous_hash: match previous_hash {
+                Some(hash) => hash,
+                None => self.hash(&self.chain[self.chain.len() - 1]),
+            },
+        };
+
+        let clone = block.clone();
+
+        self.current_transactions = Vec::new();
+        self.chain.push(block);
+
+        clone
+    }
+
+    fn new_transaction(&mut self, sender: String, recipient: String, amount: usize) -> usize {
+        self.current_transactions.push({
+            Transaction {
+                sender: sender,
+                recipient: recipient,
+                amount: amount,
+            }
+        });
+
+        let mut last_block = self.last_block();
+        last_block.index += 1;
+        last_block.index
+    }
+
+    fn last_block(self) -> Block {
+        // let clone = self.chain[self.chain.len() - 1].clone();
+        // clone
+
+        self.chain[self.chain.len() - 1]
+    }
+
+    fn hash(&self, block: &Block) -> String {
+        let mut sha256 = Sha256::new();
+        sha256.input_str("hoge");
+
+        sha256.result_str().as_bytes().to_hex()
+    }
+}
+
+
+#[derive(Clone, Debug)]
+struct Block {
+    index: usize,
+    timestamp: SystemTime,
+    transactions: Vec<Transaction>,
+    proof: usize,
+    previous_hash: String,
+}
+
+#[derive(Clone, Debug)]
+struct Transaction {
+    sender: String,
+    recipient: String,
+    amount: usize,
 }
